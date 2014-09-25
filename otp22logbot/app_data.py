1c909b26ed4abf3c6c88176c56bf333d26f8d0cc
Moo Tron <mootron@example.com>
2014-09-25 13:46:24 -0500

    set a value for timeformat_extended
    at least so example use will work

--- otp22logbot/app_data.py
+++ otp22logbot/app_data.py
@@ -2,7 +2,7 @@ APP_DATA = {
     'overlord': 'L0j1k',
     'phase': 'a',
     'timeformat': '%H:%M:%S',
-    'timeformat_extended': '',
+    'timeformat_extended': '%Y-%m-%d %H:%M:%S',
     'version': '0.0.4'
 }
 
