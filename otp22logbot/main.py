48682a9f02191a196a5c521d67256342986576ea
Moo Tron <mootron@example.com>
2014-09-25 14:36:15 -0500

    remove nargs='?'

--- otp22logbot/main.py
+++ otp22logbot/main.py
@@ -22,63 +22,54 @@ def make_parser():
         '-c', '--channel',
         help='IRC channel to join. Default "otp22"',
         default='ircugm',
-        nargs='?',
         type=str
     )
     parser.add_argument(
         '-i', '--init',
         help='Specify initialization/configuration file for logbot',
         default=False,
-        nargs='?',
         type=argparse.FileType('r')
     )
     parser.add_argument(
         '-k', '--kill',
         help='Kill password to stop bot from IRC.',
         default=None,
-        nargs='?',
         type=str
     )
     parser.add_argument(
         '-n', '--nick',
         help='IRC nick name. Default "otp22logbot"',
         default='otp22logbot',
-        nargs='?',
         type=str
     )
     parser.add_argument(
         '-o', '--output',
         help='Output log filename. Default "otp22logbot.log"',
         default='otp22logbot.log',
-        nargs='?',
         type=argparse.FileType('w')
     )
     parser.add_argument(
         '-p', '--port',
         help='IRC port to use. Default 6667',
         default=6667,
-        nargs='?',
         type=int
     )
     parser.add_argument(
         '-r', '--real',
         help='IRC real name. Default "otp22logbot"',
         default='otp22logbot',
-        nargs='?',
         type=str
     )
     parser.add_argument(
         '-s', '--server',
         help='IRC server to connect to. Default "irc.freenode.net"',
         default='localhost',
-        nargs='?',
         type=str
     )
     parser.add_argument(
         '-u', '--user',
         help='IRC user name. Default "otp22logbot"',
         default='otp22logbot',
-        nargs='?',
         type=str
     )
     parser.add_argument(
